a352dd59e8d240ae225aee9b519c42756ce37840
Drew Chandler <drechand@usbosrad8673.global.publicisgroupe.net>
2015-07-01 15:25:15 -0400

    Refactoring gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -9,32 +9,73 @@ var gulp = require('gulp'),
     livereload = require('gulp-livereload'),
     connect = require('gulp-connect'),
     wrap = require('gulp-wrap'),
+    del = require('del'),
     $ = require('gulp-load-plugins')();
 
 
 
 // ----------------------------------------
 // ----------------------------------------
+// ------- SET US UP TEH PATH VARS --------
+// ----------------------------------------
+// ----------------------------------------
+
+module.exports = {
+
+  'scripts': {
+    'src': './app/js/**/*.js',
+    'dest': './build/js/'
+  },
+
+  'images': {
+    'src': './app/images/**/*.{jpeg,jpg,png}',
+    'dest': './build/images/'
+  },
+  
+  'styles': {
+    'src': './app/styles/**/*.scss',
+    'dest': './build/css/'
+  },
+  
+  'sourceDir': './app/',
+  
+  'buildDir': './build/'
+
+}
+
+var sourceDir = './app/',
+    buildDir = './build/';
+
+
+
+
+
+// ----------------------------------------
 // ----------------------------------------
 // ------------ GULP CONNECT --------------
 // ----------------------------------------
 // ----------------------------------------
-// ----------------------------------------
+
 gulp.task('connect', function() {
+  // connect.server({
+  //   root: 'build',
+  //   livereload: true
+  // });
   connect.server();
 });
 
 
 
-// ----------------------------------------
+
+
 // ----------------------------------------
 // ----------------------------------------
 // ------------ GULP SASSY ----------------
 // ----------------------------------------
 // ----------------------------------------
-// ----------------------------------------
+
 gulp.task('sassy', function () {
-    return gulp.src(__dirname + '/app/sass/style.scss')
+    return gulp.src(sourceDir + 'sass/style.scss')
     .pipe($.plumber({ errorHandler: $.notify.onError("SASS compile has failed! <%= error %>") }))
     .pipe($.sourcemaps.init())
     .pipe($.sass({
@@ -42,30 +83,67 @@ gulp.task('sassy', function () {
         includePaths: bourbonPaths
     }))
     .pipe($.sourcemaps.write('./'))
-    .pipe(gulp.dest(__dirname + '/app/css'))
+    .pipe(gulp.dest(buildDir + 'css'))
     .pipe($.notify({'title': 'SASS good!', 'message': 'Do you even successful SASS build, bro?!'}));
 });
 
 
 
+
+
 // ----------------------------------------
+// ----------------------------------------
+// ----------- GULP SCRIPTS----------------
+// ----------------------------------------
+// ----------------------------------------
+// Will flesh this out to do more later
+
+gulp.task('scripts', function(){
+  return gulp.src(sourceDir + 'js/**/*.js')
+  .pipe(gulp.dest(buildDir + 'js'))
+});
+
+
+
+
+
 // ----------------------------------------
 // ----------------------------------------
 // ------------- GULP WRAP ----------------
 // ----------------------------------------
 // ----------------------------------------
-// ----------------------------------------
+
 gulp.task('layout', function () {
-  return gulp.src(['app/templates/**/*.html', '!app/templates/_base.html'])
-    .pipe(wrap({src: 'app/templates/_base.html'}))
-    .pipe(gulp.dest('./'))
+  return gulp.src([sourceDir + 'templates/**/*.html', !sourceDir + 'templates/_base.html'])
+    .pipe(wrap({src: sourceDir + 'templates/_base.html'}))
+    .pipe(gulp.dest(buildDir))
     .pipe($.notify({'title': 'HTML good!', 'message': 'Do you even successful HTML build, bro?!'}));
 });
 
 
 
 
-// watch
+
+// ----------------------------------------
+// ----------------------------------------
+// ------------- GULP CLEAN ---------------
+// ----------------------------------------
+// ----------------------------------------
+
+gulp.task('clean', function(cb) {
+  del(buildDir, cb);
+});
+
+
+
+
+
+// ----------------------------------------
+// ----------------------------------------
+// ------------- GULP WATCH ---------------
+// ----------------------------------------
+// ----------------------------------------
+
 gulp.task('watch', function () {
     livereload.listen();
     gulp.watch(['app/sass/**'], ['sassy']).on('change', livereload.changed);
@@ -75,10 +153,20 @@ gulp.task('watch', function () {
     gulp.watch(['app/css/**']).on('change', livereload.changed);
 });
 
-// default
+
+
+
+
+// ----------------------------------------
+// ----------------------------------------
+// --------------- DEFAULT ----------------
+// ----------------------------------------
+// ----------------------------------------
+
 gulp.task('default', [
     'connect',
     'sassy',
+    'scripts',
     'layout',
     'watch'
 ]);
