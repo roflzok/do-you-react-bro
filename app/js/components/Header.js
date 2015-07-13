ea653fc10bf5a301e9aa3c13f6d6dbd84a8793f6
Drew Chandler <drechand@usbosrad8673.global.publicisgroupe.net>
2015-07-13 10:13:30 -0400

    First commit

---
+++ app/js/components/Header.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var React = require('react/addons');
+
+var Header = React.createClass({
+
+  render: function() {
+    return (
+      <header>
+
+        Header
+
+      </header>
+    );
+  }
+
+});
+
+module.exports = Header;
